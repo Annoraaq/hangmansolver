89ec809a4736929969c0352c6d4e5b74c1a4e90f
Johannes Baum <baum@bw-webgestaltung.de>
2018-12-25 11:52:38 +0100

    add solver

---
+++ HangmanSolver.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class HangmanSolver {
+
+  static Scanner sc = new Scanner(System.in);
+
+  public static void main(String args[]) {
+    String input;
+    int guesses = 6;
+    String toGuess = "etaoinshr";
+    boolean isGame = true;
+
+    while (isGame) {
+      input = sc.nextLine();
+      if (!input.contains("_")) {
+        // input = sc.nextLine();
+      }
+      System.out.println(""+toGuess.charAt(guesses-1));
+      guesses--;
+      if (guesses <= 0) {
+        // input = sc.nextLine();
+        guesses = 6;
+      }
+    }
+  }
+
+  private static boolean containsOnlyUnderscores(String input) {
+    for (int i=0; i<input.length(); i++) {
+      if (input.charAt(i) != '_') {
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
