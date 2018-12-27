afbf108345cc6af66e0333ed6b6986a54e7d94db
Johannes Baum <baum@bw-webgestaltung.de>
2018-12-27 19:46:49 +0100

    use english letter frequency

--- HangmanSolver.java
+++ HangmanSolver.java
@@ -5,32 +5,30 @@ public class HangmanSolver {
   static Scanner sc = new Scanner(System.in);
 
   public static void main(String args[]) {
-    String input;
-    int guesses = 26;
+    int guessesLeft = 6;
+    int letter = 0;
     String toGuess = "etaoinshrdlcumwfgypbvkjxqz";
-    boolean isGame = true;
+    String lastRound = sc.nextLine();
 
-    while (isGame) {
-      input = sc.nextLine();
-      if (!input.contains("_")) {
-        // input = sc.nextLine();
-      }
-      System.out.println(""+toGuess.charAt(guesses-1));
-      guesses--;
-      if (guesses <= 0) {
-        // input = sc.nextLine();
-        guesses = 26;
-      }
-    }
-  }
+    while (true) {
+      System.out.println(""+toGuess.charAt(letter));
+      letter++;
 
-  private static boolean containsOnlyUnderscores(String input) {
-    for (int i=0; i<input.length(); i++) {
-      if (input.charAt(i) != '_') {
-        return false;
+      if (letter >= 25) {
+        letter = 0;
       }
+
+      String input = sc.nextLine();
+      if (lastRound.equals(input)) {
+        guessesLeft--;
+      }
+      if (guessesLeft < 1 || !input.contains("_")) {
+        guessesLeft = 6;
+        letter = 0;
+        input = sc.nextLine();
+      }
+      lastRound = input;
     }
-    return true;
   }
 
 }
